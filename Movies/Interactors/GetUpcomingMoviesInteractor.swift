758cc9b44fafd31011e878c10146b0f8415a0c56
Allan Melo <allan@enjoei.com.br>
2019-06-14 02:04:45 -0300

    Create GetUpcomingMoviesInteractor and their tests

---
+++ Movies/Interactors/GetUpcomingMoviesInteractor.swift
@@ -0,0 +1,46 @@
+//
+//  GetUpcomingMoviesInteractor.swift
+//  Movies
+//
+//  Created by Allan Melo on 14/06/19.
+//  Copyright © 2019 Allan Melo. All rights reserved.
+//
+
+import RxSwift
+
+class GetUpcomingMoviesInteractor {
+    let page: Int
+
+    init(page: Int = 1) {
+        self.page = page
+    }
+
+    func execute() -> Observable<UpcomingMoviesResult> {
+        return createObservable()
+    }
+}
+
+extension GetUpcomingMoviesInteractor: LoaderCodableObservable {
+    typealias T = UpcomingMoviesResult
+
+    func getUrl() -> String {
+        return "https://api.themoviedb.org/3/movie/upcoming?api_key=\(Settings.API_KEY)&page=\(page)"
+    }
+}
+
+struct UpcomingMoviesResult: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int64
+    let title: String
+    let voteCount: Int64
+    let popularity: Double
+    let posterPath: String
+    let backdropPath: String
+    let releaseDate: String
+    let genreIds: [Int]
+    let overview: String
+    let originalLanguage: String
+}
---
+++ UnitTests/Interactors/GetUpcomingMoviesTest.swift
@@ -0,0 +1,31 @@
+//
+//  GetUpcomingMoviesTest.swift
+//  Movies
+//
+//  Created by Allan Melo on 14/06/19.
+//  Copyright © 2019 Allan Melo. All rights reserved.
+//
+
+@testable import Movies
+
+import XCTest
+
+class GetUpcomingMoviesTest: XCTestCase {
+
+    func testGetUpcomingMoviesInteractor() {
+        let interactor = GetUpcomingMoviesInteractor()
+        let result = interactor.execute().result()
+
+        XCTAssertNotNil(result)
+        XCTAssertFalse(result!.results.isEmpty)
+    }
+
+    func testGetUpcomingMoviesInteractorPage2() {
+        let interactor = GetUpcomingMoviesInteractor(page: 2)
+        let result = interactor.execute().result()
+
+        XCTAssertNotNil(result)
+        XCTAssertFalse(result!.results.isEmpty)
+    }
+}
+
