f7186a50192d739ed228692076160f1e13f4f092
Allan Melo <allan@enjoei.com.br>
2019-06-15 09:26:24 -0300

    Create TokenView component

---
+++ Movies/SharedCells/TokenCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  TokenCollectionViewCell.swift
+//  Movies
+//
+//  Created by Allan Melo on 15/06/19.
+//  Copyright © 2019 Allan Melo. All rights reserved.
+//
+
+import UIKit
+
+class TokenCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var containerView: UIView?
+    @IBOutlet weak var titleLabel: UILabel?
+
+    var measuredSize: CGSize {
+        let maxWidth: CGFloat = 200
+        let lineHeight: CGFloat = 13
+        let paddingText: CGFloat = 10
+        let tokenHeight: CGFloat = 20
+
+        guard let text = titleLabel?.text,
+            let font = titleLabel?.font else {
+            return CGSize.zero
+        }
+
+        let constraint = CGSize(width: maxWidth, height: lineHeight)
+        let context = NSStringDrawingContext()
+
+        let boundingBox = text
+            .boundingRect(with: constraint,
+                          options: .usesLineFragmentOrigin,
+                          attributes: [NSAttributedString.Key.font: font],
+                          context: context).size
+
+        return CGSize(width: boundingBox.width + paddingText, height: tokenHeight)
+    }
+
+    override func awakeFromNib() {
+        containerView?.layer.borderColor = UIColor.white.cgColor
+    }
+
+    func configure(title: String) {
+        titleLabel?.text = title
+    }
+}
---
+++ Movies/SharedViews/TokensView.swift
@@ -0,0 +1,75 @@
+//
+//  TokensView.swift
+//  Movies
+//
+//  Created by Allan Melo on 15/06/19.
+//  Copyright © 2019 Allan Melo. All rights reserved.
+//
+
+import UIKit
+import AlignedCollectionViewFlowLayout
+
+class TokensView: UICollectionView {
+    let cell = TokenCollectionViewCell.className
+
+    var tokens: [String] = [] {
+        didSet {
+            reloadData()
+        }
+    }
+
+    required init?(coder ADecoder: NSCoder) {
+        super.init(coder: ADecoder)
+
+        setup()
+    }
+
+    func setup() {
+        let tokenNib = UINib(nibName: cell, bundle: nil)
+        register(tokenNib, forCellWithReuseIdentifier: cell)
+
+        delegate = self
+        dataSource = self
+
+        let alignedFlowLayout = AlignedCollectionViewFlowLayout(horizontalAlignment: .left, verticalAlignment: .center)
+
+        alignedFlowLayout.minimumInteritemSpacing = 3
+
+        collectionViewLayout = alignedFlowLayout
+    }
+}
+
+extension TokensView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tokens.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)
+        -> UICollectionViewCell {
+
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cell, for: indexPath)
+            as? TokenCollectionViewCell,
+            let genre = tokens.get(at: indexPath.row) else {
+                return UICollectionViewCell()
+        }
+
+        cell.configure(title: genre)
+
+        return cell
+    }
+}
+
+extension TokensView: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        guard let cell = self.collectionView(collectionView, cellForItemAt: indexPath)
+            as? TokenCollectionViewCell else {
+                return CGSize.zero
+        }
+
+        return cell.measuredSize
+    }
+}
+
