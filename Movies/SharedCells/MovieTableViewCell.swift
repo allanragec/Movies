9c139c9a872783cd7d5e14bebf24464216030888
Allan Melo <allan@enjoei.com.br>
2019-06-16 16:24:09 -0300

    Hide poster image of MovieTableViewCell if movie don't have image

--- Movies/SharedCells/MovieTableViewCell.swift
+++ Movies/SharedCells/MovieTableViewCell.swift
@@ -44,6 +44,7 @@ class MovieTableViewCell: UITableViewCell {
     @IBOutlet weak var releaseDateLabel: UILabel?
     @IBOutlet weak var tokenView: TokensView?
     @IBOutlet weak var userScoreCircularView: UICircularProgressRing?
+    @IBOutlet weak var coverWidthConstraint: NSLayoutConstraint?
 
     var genres: [String] = [] {
         didSet {
@@ -65,16 +66,21 @@ class MovieTableViewCell: UITableViewCell {
 
     func configure(movie: Movie) {
         titleLabel?.text = movie.title
-        releaseDateLabel?.text = "Release \(movie.releaseDate)"
+        releaseDateLabel?.text = movie.releaseDate.isEmpty ?
+            "" : "Release \(movie.releaseDate)"
         genres = Settings.genres?.getTitles(ids: movie.genreIds) ?? []
-
-        guard let posterPath = movie.posterPath else { return }
-
-        coverImageView?.sd_setImage(with: URL(string: "https://image.tmdb.org/t/p/w200/\(posterPath)"))
 
         userScoreCircularView?.valueFormatter = VoteAverageFormatter()
         userScoreCircularView?.style = .ontop
         userScoreCircularView?.font = UIFont.boldSystemFont(ofSize: 9)
         userScoreCircularView?.value = CGFloat(movie.voteAverage)
+
+        guard let posterPath = movie.posterPath else {
+            coverWidthConstraint?.constant = 0
+            return
+        }
+
+        coverWidthConstraint?.constant = 82
+        coverImageView?.sd_setImage(with: URL(string: "https://image.tmdb.org/t/p/w200/\(posterPath)"))
     }
 }
