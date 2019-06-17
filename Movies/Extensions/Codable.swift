0fd64c21d396b0e249af360782550e2047d56cdd
Allan Melo <allan@enjoei.com.br>
2019-06-17 02:22:46 -0300

    Source formatting

--- Movies/Extensions/Codable.swift
+++ Movies/Extensions/Codable.swift
@@ -66,7 +66,8 @@ extension Encodable {
             encoder.keyEncodingStrategy = .convertToSnakeCase
             let data = try encoder.encode(self)
 
-            guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                as? [String: Any] else {
                 return [String: Any]()
             }
             return dictionary
